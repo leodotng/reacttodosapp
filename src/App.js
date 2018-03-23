ed501cc4b6c6c6028b1fbda48377a89c019e6adf
leodotng <josh@leo.ng>
2018-03-22 20:34:02 -0600

    added netlify deploy support

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Button, Card, Icon } from "semantic-ui-react";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button color="green"><Icon name="sign in" size="small" />Login</Button>
+        <Button color="green"><Icon name="signup" size="small" />Signup</Button>
+
+        <h1 className="App-title">Todos App</h1>
+        <Card color="green" centered="true">
+          <p>Add New Todo</p>
+          <Icon name="plus square" size="large" />
+        </Card>
+        <Card color="green" centered="true">
+          <p>Take out the Laundry</p>
+          <Icon name="delete" size="large" />
+        </Card>
+        <Card color="green" centered="true">
+          <p>Take out the trash</p>
+          <Icon name="delete" size="large" />
+        </Card>
+        <Card color="green" centered="true">
+          <p>Feed The Cats</p>
+          <Icon name="delete" size="large" />
+        </Card>
       </div>
     );
   }
