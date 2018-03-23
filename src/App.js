3f9436dcfef889bd8b445b29dc8703ba71d2cb42
leodotng <josh@leo.ng>
2018-03-22 22:17:23 -0600

    added facebook and github login oauth

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 import { Button, Card, Icon } from "semantic-ui-react";
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button color="green"><Icon name="sign in" size="small" />Login</Button>
-        <Button color="green"><Icon name="signup" size="small" />Signup</Button>
+      
+        <Button color="green" size="large">
+          <Icon name="sign in" size="small" />Login
+        </Button>
+        <Button color="green" size="large">
+          <Icon name="signup" size="small" />Signup
+        </Button>
+        <br />
 
         <h1 className="App-title">Todos App</h1>
+        <h2>Welcome to the best Todos App!</h2>
         <Card color="green" centered="true">
           <p>Add New Todo</p>
           <Icon name="plus square" size="large" />
@@ -27,6 +33,9 @@ class App extends Component {
           <p>Feed The Cats</p>
           <Icon name="delete" size="large" />
         </Card>
+        <Button color="facebook">
+          <Icon name="facebook" /> Facebook
+        </Button>
       </div>
     );
   }
